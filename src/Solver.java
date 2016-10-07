b12eba4c90ca1fdcdb49ebff408546c6471cb745
Kasama <ralegro@bol.com.br>
2016-10-07 16:06:20 -0300

    updated gitignore and started to work on reverse solution tree

--- src/Solver.java
+++ src/Solver.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+
 public class Solver {
 
 	private Cube cube;
 
 	public Solver(Cube cube){
 		this.cube = cube;
+	}
+
+	public ArrayList<Cube> generateReverseSolutionTree(){
+
+		ArrayList<Cube> cubes = new ArrayList<>();
+
+		Cube baseCube = new Cube();
+		cubes.add(baseCube);
+
+		return cubes;
 	}
 
 
